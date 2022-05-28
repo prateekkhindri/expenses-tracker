1c5188ce5072208a8b34f8d013bedbe5bac26bb6
Prateek Khindri <prateek.khindri90@gmail.com>
2022-05-29 08:53:17 +1000

    Created userSchema

---
+++ api/src/modules/user/User.schema.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
+    email: {
+      type: String,
+      required: true,
+      maxLength: [50, "Name must be less than 50 characters"],
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [5, "Password must be less than 50 characters"],
+      maxLength: [50, "Password must be less than 50 characters"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// We just created a new schema called UserSchema, now time to convert
+mongoose.model("User", UserSchema); //Users
