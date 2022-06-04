7d9fcc252dc2b18949d591bccc1183d8d6611d50
Prateek Khindri <prateek.khindri90@gmail.com>
2022-06-04 13:58:10 +1000

    Dashboard UI updated

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import { useNavigate } from "react-router-dom";
+import { Button, Col, Form, Row } from "react-bootstrap";
 
 // const auth = false;
 export const Dashboard = () => {
   const navigate = useNavigate();
+
+  const [form, setForm] = useState("");
 
   // const [user, setUser] = useState();
 
@@ -14,5 +17,53 @@ export const Dashboard = () => {
 
     !storedUser?._id && navigate("/");
   }, []);
-  return <Layout>Dashboard Page</Layout>;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  return (
+    <Layout>
+      <Form className="mt-5">
+        <Row className="g-2">
+          <Col md={2}>
+            <Form.Select name="type" onChange={handleOnChange}>
+              <option value="">Choose...</option>
+              <option value="income">Income</option>
+              <option value="expenses">Expenses</option>
+            </Form.Select>
+          </Col>
+          <Col md={6}>
+            <Form.Control
+              onChange={handleOnChange}
+              name="title"
+              placeholder="Transaction title"
+              required
+            />
+          </Col>
+          <Col md={2}>
+            <Form.Control
+              onChange={handleOnChange}
+              name="amount"
+              placeholder="50"
+              type="number"
+              required
+            />
+          </Col>
+          <Col md={2}>
+            <Button variant="primary" type="submit" className="form-control">
+              Add
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+
+      <hr />
+    </Layout>
+  );
 };
